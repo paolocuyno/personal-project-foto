36be11a901ca7e6b3ed0d07e2fc471aecef06116
Paolo Cuyno <paolo.cuyno@gmail.com>
2021-04-23 06:14:31 -0700

    hosting with domain attempt 2

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import Post from './Components/Post/Post'
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
 import Update from './Components/Update'
-import {HashRouter as Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Switch, Route} from 'react-router-dom'
 
 export default (
     <Switch>
