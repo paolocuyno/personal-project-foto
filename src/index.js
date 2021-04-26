e21f31d4763ba6221771a7a30fe39fa21ec8193b
Paolo Cuyno <paolo.cuyno@gmail.com>
2021-04-26 09:24:35 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter, HashRouter} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-const Router=process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter
 
 ReactDOM.render(
   <React.StrictMode>
